64650239768ae506fe212680b67a8353eef0a83d
Dagim <dagimed6@gmail.com>
2025-07-20 20:28:23 +0400

    task 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .models import Library 
+from django.contrib.auth.forms import  UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 
 # User login view
