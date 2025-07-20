d6475620e2614558beb1124857d53f6c5ea85cd3
Dagim <dagimed6@gmail.com>
2025-07-20 20:56:44 +0400

    add another pages

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,4 +17,8 @@ urlpatterns = [
 
     # Registration still uses custom view
     path('register/', views.register_view, name='register'),
+
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,8 +2,11 @@ from django.contrib.auth import login, authenticate, logout
 from .models import Library 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import  AuthenticationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+from .models import UserProfile
 
 from .models import Book  
 def list_books(request):
@@ -14,6 +17,33 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')   
+
+   
     # User login view
 def login_view(request):
     if request.method == 'POST':
