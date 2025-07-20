2d2364d47607625c2f130d035a4568880b4262e1
Dagim <dagimed6@gmail.com>
2025-07-20 20:41:12 +0400

    fix pervious tasks

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # ✅ Auth views using built-in LoginView & LogoutView (required by checker)
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
