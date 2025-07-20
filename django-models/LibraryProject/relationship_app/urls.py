493b87910f559b2e619617181d8c8211516c87a6
Dagim <dagimed6@gmail.com>
2025-07-20 20:45:41 +0400

    fix pervious tasks

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+
+from Introduction_to_Django.LibraryProject.bookshelf import admin
 from .views import list_books
 from . import views
 
 urlpatterns = [
     # Function & class-based views from earlier
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,18 @@ from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import  AuthenticationForm
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
 
-# User login view
+from .models import Book  
+def list_books(request):
+    books = Book.objects.all()  
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+    # User login view
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
