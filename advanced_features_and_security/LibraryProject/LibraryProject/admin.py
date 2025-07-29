cc3958f354c95fca027aa6926e5af540aa8659a6
Dagim <dagimed6@gmail.com>
2025-07-29 16:44:29 +0400

    fix admin

--- advanced_features_and_security/LibraryProject/LibraryProject/admin.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Book
 
-
+# Define a custom admin class
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
@@ -13,5 +13,6 @@ class CustomUserAdmin(UserAdmin):
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
 
-
+# Register the custom user and the Book model
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
